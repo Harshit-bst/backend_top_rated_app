2d0f8afb0a62807ede20c308d6f7a8497bc85a52
Harshit Agarwal <harshit@Harshits-MacBook-Pro.local>
2022-04-19 09:48:41 +0530

    changes to enable deployment to gae

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
 DEBUG=True
 SECRET_KEY="blablablalVladamirPutin*****"
-SQLALCHEMY_DATABASE_URI="postgresql://harshit:devpassword@postgres:5432/backend_top_rated_app"
+SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://harshit:devpassword@/backend_top_rated_app?host=/cloudsql/bluestacks-cloud-beginners:us-central1:backend-top-rated-app-pg-instance"
 SQLALCHEMY_TRACK_MODIFICATIONS=True
